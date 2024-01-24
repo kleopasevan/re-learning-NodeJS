48a96c1db78a1de93a4a6f65d2a900879ffd5635
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-01-24 17:41:38 +0700

    Learning  the fundamentals of Node
    Node globals

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const total = 9
+    if (total < 10) {
+        console.log ('small number')
+    } else {
+        console.log ('large number')
+    }
+    console.log ("first app")
---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+//GLOBALS
+ // __dirname - path to current directory
+ // __filename - file name
+ // require - function to use modules
+ // module - info about current module
+ // process - info about env where the progrem is being executed
+ 
+console.log(__dirname);
+setInterval(()=>{
+    console.log('Hello World')
+}, 1000)
